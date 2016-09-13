2f7922ceeb4ccb9ce1420ca7e13fa5880ecbd78e
Pierre PIRONIN <pironin.pierre@gmail.com>
2016-09-13 22:16:19 +0200

    First attempt to fill books from web API.

--- index.ios.js
+++ index.ios.js
@@ -4,50 +4,70 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    View,
+    ListView,
+    Text
 } from 'react-native';
 
-class MicoseMobile extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+import Spinner from 'react-native-loading-spinner-overlay';
+
+class Book extends Component {
+    constructor() {
+        super()
+    }
+
+    render() {
+        return (
+            <Text style={{flex: 1}}>Hello {this.props.title}</Text>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+class MicoseMobile extends Component {
+    constructor(props) {
+        super(props);
+
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.title !== r2.title});
+
+        this.state = {
+            loading: true,
+            dataSource: ds
+        }
+    }
+
+    componentDidMount() {
+        this.fetchBooks();
+    }
+
+    fetchBooks() {
+        fetch('https://www.micose.pierrepironin.fr/api/books/')
+            .then((response) => response.json())
+            .then((responseData) => {
+                const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.title !== r2.title});
+                this.setState({
+                    dataSource: ds.cloneWithRows(responseData)
+                })
+            })
+            .catch((error) => {
+                console.warn(error);
+            })
+            .done();
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <Spinner visible={this.state.loading}/>
+                <ListView
+                    dataSource={this.state.dataSource}
+                    renderRow={(rowData) => <Book title={rowData.title}/>}
+                />
+            </View>
+        );
+    }
+}
 
 AppRegistry.registerComponent('MicoseMobile', () => MicoseMobile);
