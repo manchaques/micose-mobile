732f929e949ac6aa5ec60d077b07e5a05d7c95ea
Pierre PIRONIN <pironin.pierre@gmail.com>
2016-10-04 21:24:58 +0200

    Refactor index to share same root component on Android and iOS.

---
+++ components/app.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import {
+    AppRegistry,
+    View,
+    ListView,
+    StyleSheet
+} from 'react-native';
+
+import Spinner from 'react-native-loading-spinner-overlay';
+import moment from 'moment';
+
+import Book from './book';
+
+const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.title !== r2.title});
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            loading: true,
+            dataSource: ds.cloneWithRows([])
+        }
+    }
+
+    componentDidMount() {
+        this.fetchBooks();
+    }
+
+    fetchBooks() {
+        fetch('https://www.micose.pierrepironin.fr/api/books/')
+        // fetch('http://192.168.0.12:4000/api/books/')
+            .then((response) => {
+                return response.json();
+            })
+            .then((response) => {
+                // Sort by update date
+                return response.data.sort((book1, book2) => {
+                    moment(book1.updateDate).isBefore(book2.updateDate)
+                }).reverse();
+            })
+            .then((books) => {
+                this.setState({
+                    loading: false,
+                    dataSource: ds.cloneWithRows(books)
+                });
+            })
+            .catch((error) => {
+                console.warn(error);
+            })
+            .done();
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <Spinner visible={this.state.loading}/>
+                <ListView
+                    style={styles.container}
+                    dataSource={this.state.dataSource}
+                    renderRow={(rowData) => <Book book={rowData} />}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 20,
+    },
+});
+
+export default App;
--- index.ios.js
+++ index.ios.js
@@ -1,70 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, {Component} from 'react';
 import {
     AppRegistry,
-    View,
-    ListView,
-    Text
+    View
 } from 'react-native';
 
-import Spinner from 'react-native-loading-spinner-overlay';
-
-class Book extends Component {
-    constructor() {
-        super()
-    }
-
-    render() {
-        return (
-            <Text style={{flex: 1}}>Hello {this.props.title}</Text>
-        );
-    }
-}
+import App from './components/app';
 
 class MicoseMobile extends Component {
-    constructor(props) {
-        super(props);
-
-        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.title !== r2.title});
-
-        this.state = {
-            loading: true,
-            dataSource: ds
-        }
-    }
-
-    componentDidMount() {
-        this.fetchBooks();
-    }
-
-    fetchBooks() {
-        fetch('https://www.micose.pierrepironin.fr/api/books/')
-            .then((response) => response.json())
-            .then((responseData) => {
-                const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.title !== r2.title});
-                this.setState({
-                    dataSource: ds.cloneWithRows(responseData)
-                })
-            })
-            .catch((error) => {
-                console.warn(error);
-            })
-            .done();
-    }
-
     render() {
         return (
             <View style={{flex: 1}}>
-                <Spinner visible={this.state.loading}/>
-                <ListView
-                    dataSource={this.state.dataSource}
-                    renderRow={(rowData) => <Book title={rowData.title}/>}
-                />
+                <App/>
             </View>
         );
     }
